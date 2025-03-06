fcdcc1b90e855898921bea43367f3fac9d87f2d9
pointcrash <r.s.ricce@gmail.com>
2025-03-06 16:25:46 +0100

    add rel to Package from PackageType

--- fastapi-application/core/models/package_type.py
+++ fastapi-application/core/models/package_type.py
@@ -1,3 +1,5 @@
+from typing import TYPE_CHECKING
+
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
@@ -5,5 +7,10 @@ from core.models.base import Base
 from core.models.mixins.int_id_pk import IntIdPkMixin
 
 
+if TYPE_CHECKING:
+    from .package import Package
+
+
 class PackageType(IntIdPkMixin, Base):
     name: Mapped[str] = mapped_column(String(50), unique=True)
+    packages: Mapped[list["Package"]] = relationship(back_populates="type")
