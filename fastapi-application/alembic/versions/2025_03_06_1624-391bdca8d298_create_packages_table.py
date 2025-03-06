8ccef8084d81ad4e67af67cc02e3899f1d0e8f3d
pointcrash <r.s.ricce@gmail.com>
2025-03-06 16:25:16 +0100

    create packages table migrate

---
+++ fastapi-application/alembic/versions/2025_03_06_1624-391bdca8d298_create_packages_table.py
@@ -0,0 +1,48 @@
+"""create packages table
+
+Revision ID: 391bdca8d298
+Revises: 88c877e88603
+Create Date: 2025-03-06 16:24:22.539462
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+revision: str = "391bdca8d298"
+down_revision: Union[str, None] = "88c877e88603"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "packages",
+        sa.Column("session_id", sa.String(length=100), nullable=False),
+        sa.Column("name", sa.String(length=255), nullable=False),
+        sa.Column("weight", sa.DECIMAL(precision=10, scale=2), nullable=False),
+        sa.Column("content_cost", sa.DECIMAL(precision=10, scale=2), nullable=False),
+        sa.Column("delivery_cost", sa.DECIMAL(precision=10, scale=2), nullable=False),
+        sa.Column("type_id", sa.Integer(), nullable=False),
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.ForeignKeyConstraint(
+            ["type_id"],
+            ["package_types.id"],
+            name=op.f("fk_packages_type_id_package_types"),
+        ),
+        sa.PrimaryKeyConstraint("id", name=op.f("pk_packages")),
+    )
+    op.create_index(
+        op.f("ix_packages_session_id"),
+        "packages",
+        ["session_id"],
+        unique=False,
+    )
+
+
+def downgrade() -> None:
+    op.drop_index(op.f("ix_packages_session_id"), table_name="packages")
+    op.drop_table("packages")
--- fastapi-application/core/models/__init__.py
+++ fastapi-application/core/models/__init__.py
@@ -2,8 +2,10 @@ __all__ = (
     "db_helper",
     "Base",
     "PackageType",
+    "Package",
 )
 
 from .db_helper import db_helper
 from .base import Base
+from .package import Package
 from .package_type import PackageType
